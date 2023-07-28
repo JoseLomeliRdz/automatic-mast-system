e7129760b05f1ca4d5dadd9f1ae208c1c321e963
JoseLomeliRdz <joselomeli9927@gmail.com>
2023-07-27 23:02:51 -0600

    New Configuration for Lidar

--- scripts/HMI.py
+++ scripts/HMI.py
@@ -29,8 +29,8 @@ class HMI(QMainWindow):
         self.serial.readyRead.connect(self.leer_serial) # Connect the serial.readyRead signal to the leer_serial function
 
         # Connect the buttons from the Lidar Frame to their respective functions
-        self.bt_iniciar_lidar.clicked.connect(lambda: self.enviar_serial("Enviando instrucción: Iniciar Lidar", 0x01)) # Connect the iniciar_lidar button to the iniciar_lidar function
-        self.bt_detener_lidar.clicked.connect(lambda: self.enviar_serial("Enviando instrucción: Detener Lidar", 0x02)) # Connect the pausar_lidar button to the pausar_lidar function
+        self.bt_iniciar_lidar.clicked.connect(lambda: self.enviar_serial("Enviando instrucción: Iniciar Lidar", 1)) # Connect the iniciar_lidar button to the iniciar_lidar function
+        self.bt_detener_lidar.clicked.connect(lambda: self.enviar_serial("Enviando instrucción: Detener Lidar", 2)) # Connect the pausar_lidar button to the pausar_lidar function
         self.bt_guardar_lidar.clicked.connect(self.guardar_lidar) # Connect the guardar_lidar button to the guardar_lidar function
 
         # Connect the Mode Selection buttons to their respective functions
@@ -39,21 +39,21 @@ class HMI(QMainWindow):
         
         # Connect the buttons from the Modo Manual Frame to their respective functions
         self.bt_enviar_manual.clicked.connect(self.enviar_manual_data) # Connect the enviar_manual button to the enviar_manual_func function
-        self.bt_home_tilt.clicked.connect(lambda: self.enviar_serial("Enviando instrucción: Posición Home Cabezal",0x06)) # Connect the home_tilt button to the home_tilt function
-        self.bt_ascenso.clicked.connect(lambda: self.enviar_serial("Enviando instrucción: Ascenso:",0x07)) # Connect the ascenso button to the ascenso function
-        self.bt_descenso.clicked.connect(lambda: self.enviar_serial("Enviando instrucción: Descenso:",0x08)) # Connect the descenso button to the descenso function
+        self.bt_home_tilt.clicked.connect(lambda: self.enviar_serial("Enviando instrucción: Posición Home Cabezal",6)) # Connect the home_tilt button to the home_tilt function
+        self.bt_ascenso.clicked.connect(lambda: self.enviar_serial("Enviando instrucción: Ascenso:",7)) # Connect the ascenso button to the ascenso function
+        self.bt_descenso.clicked.connect(lambda: self.enviar_serial("Enviando instrucción: Descenso:",8)) # Connect the descenso button to the descenso function
 
         # Connect the buttons from the Modo Automatico Frame to their respective functions
         self.bt_iniciar_rutina.clicked.connect(self.iniciar_rutina) # Connect the iniciar_rutina button to the iniciar_rutina function
 
         # Connect the signals from the Modo Automatico Range Selection to the PlotWidget
-        self.entry_inf_tilt.valueChanged.connect(self.update_plot) # Connect the entry_inf_tilt signal to the update_plot function
-        self.entry_sup_tilt.valueChanged.connect(self.update_plot) # Connect the entry_sup_tilt signal to the update_plot function
-        self.entry_inf_pan.valueChanged.connect(self.update_plot) # Connect the entry_inf_pan signal to the update_plot function
-        self.entry_sup_pan.valueChanged.connect(self.update_plot) # Connect the entry_sup_pan signal to the update_plot function
+        self.entry_inf_tilt.valueChanged.connect(self.actualizar_grafica) # Connect the entry_inf_tilt signal to the update_plot function
+        self.entry_sup_tilt.valueChanged.connect(self.actualizar_grafica) # Connect the entry_sup_tilt signal to the update_plot function
+        self.entry_inf_pan.valueChanged.connect(self.actualizar_grafica) # Connect the entry_inf_pan signal to the update_plot function
+        self.entry_sup_pan.valueChanged.connect(self.actualizar_grafica) # Connect the entry_sup_pan signal to the update_plot function
 
         self.leer_puertos() # Call the leer_puertos function to obtain the available ports
-        self.initialize_plot() # Call the update_plot function to initialize the plot
+        self.inicializar_grafica() # Call the update_plot function to initialize the plot
         self.seleccionar_modo('manual') # Call the seleccionar_modo function to initialize the mode to manual
 
     def leer_puertos(self):
@@ -106,7 +106,7 @@ class HMI(QMainWindow):
             print('Error al enviar datos por el puerto serial')
 
     def guardar_lidar(self): # Function to save the Lidar data
-        self.enviar_serial("Enviando instrucción: Guardar Lidar", 0x03)
+        self.enviar_serial("Enviando instrucción: Guardar Lidar", 3)
         #Codigo
     
     def seleccionar_modo(self, seleccion): # Function to select the mode of the system, it will habilitate or disable the respective frames buttons
@@ -115,29 +115,36 @@ class HMI(QMainWindow):
         elif(seleccion == 'auto'):
             self.modo = 'A'
     
+    def actualizar_display(self):
+        self.disp_tilt.display(self.tilt)
+        self.disp_pan.display(self.pan)
+
     def enviar_manual_data(self): # Function to send the manual data to the serial port
         if(self.modo == 'M'):
-            self.enviar_serial("Enviando instrucción: Enviar Pos. Manual", 0x05)
-            tilt = self.entry_tilt_manual.text()
-            pan = self.entry_pan_manual.text()
-            self.disp_tilt.display(int(tilt))
-            self.disp_pan.display(int(pan))
-            self.enviar_serial("Enviando posición manual:",bytes(str(tilt)+','+str(pan),'utf-8'))
+            self.enviar_serial("Enviando instrucción: Enviar Pos. Manual", 5)
+            self.tilt = self.entry_tilt_manual.value()
+            self.pan = self.entry_pan_manual.value()
+            self.actualizar_display()
+            data = str(self.tilt)+','+ str(self.pan)
+            data = data.encode('utf-8')
+            print("data:",data)
+            if self.serial.isOpen():
+                self.serial.write(data)
         else:
             print('Te encuentras en modo automatico, cambia a modo manual para enviar datos')
 
     def iniciar_rutina(self): # Function to send the initiate signal for the scanning routine to the serial port
         if(self.modo == 'A'):
-            self.enviar_serial("Enviando instrucción: Iniciar Rutina", 0x04)
+            self.enviar_serial("Enviando instrucción: Iniciar Rutina", 4)
             inf_tilt = self.entry_inf_tilt.value()
             sup_tilt = self.entry_sup_tilt.value()
             inf_pan = self.entry_inf_pan.value()
             sup_pan = self.entry_sup_pan.value()
-            self.enviar_serial("Enviando datos de la rutina:",bytes(str(inf_tilt)+','+str(sup_tilt)+','+str(inf_pan)+','+str(sup_pan)))
+            self.enviar_serial("Enviando datos de la rutina:",str(inf_tilt)+','+str(sup_tilt)+','+str(inf_pan)+','+str(sup_pan))
         else:
             print('Te encuentras en modo manual, cambia a modo automatico para iniciar la rutina')
 
-    def initialize_plot(self):
+    def inicializar_grafica(self):
         self.range_plot.setAspectLocked() # Lock the aspect ratio of the plot
         self.range_plot.hideAxis('bottom') # Hide the bottom axis
         self.range_plot.hideAxis('left') # Hide the left axis
@@ -171,9 +178,9 @@ class HMI(QMainWindow):
         mast_point = (0,0)
         self.range_plot.plot(mast_point,mast_point, pen=None, symbol='o', symbolSize=25, symbolBrush='y') # Add the mast point
         
-    def update_plot(self):
+    def actualizar_grafica(self):
         self.range_plot.clear() # Clear the plot
-        self.initialize_plot() # Initialize the plot
+        self.inicializar_grafica() # Initialize the plot
         radius = 250 # Set the radius of the plot
         
         # Create the pan cartesian coordinates
@@ -196,6 +203,9 @@ class HMI(QMainWindow):
         self.range_plot.addItem(pov_inf)
         pov_sup = pg.InfiniteLine((0,0),angle=sup_pan,pen=1)
         self.range_plot.addItem(pov_sup)
+    
+    
+    
 
         
     
--- scripts/mast_main/mast_main.ino
+++ scripts/mast_main/mast_main.ino
@@ -2,23 +2,74 @@
 #include <Adafruit_PWMServoDriver.h> // Libreria para controlar el driver PCA9685
 #include "RPLidar.h" // Libreria para controlar el sensor Lidar
 
+/* Setup para Servos */
 Adafruit_PWMServoDriver pwmServos = Adafruit_PWMServoDriver(); // Crear objeto pwm para controlar los servos mediante el driver PCA9685
-Adafruit_PWMServoDriver pwmLidar = Adafruit_PWMServoDriver(); // Crear objeto pwm para controlar el motor del lidar mediante el driver PCA9685
+//Adafruit_PWMServoDriver pwmLidar = Adafruit_PWMServoDriver(); // Crear objeto pwm para controlar el motor del lidar mediante el driver PCA9685
 
 #define MIN_PULSE_WIDTH 650   // Definimos el ancho de pulso minimo para el pwm
 #define MAX_PULSE_WIDTH 2350  // Definimos el ancho de pulso maximo para el pwm
 #define FREQUENCY 50          // Definimos la frecuencia del pwm
 
-uint8_t servo_pan = 0; // Definimos el servo pan
-uint8_t servo_tilt = 1; // Definimos el servo tilt
+uint8_t servo_pan = 11; // Definimos el servo pan
+uint8_t servo_tilt = 8; // Definimos el servo tilt
 
+/* Setup para Lidar */
 RPLidar lidar;
-#define RPLIDAR_MOTOR 3 // Definimos el pin del motor del sensor Lidar
+#define RPLIDAR_MOTOR_EN 3 // Definimos el pin del motor del sensor Lidar
+#define RPLIDAR_MOTOR_IN1 4 
 float minDistance = 100000;
 float angleAtMinDist = 0;
-uint8_t time = 0;
+
+/* Variables Globales */
 byte comando = 0x00;
-/*Funcion MoverServo que mueve un especificado servo a un angulo especifico*/
+int pan = 0, tilt = 0;
+
+/* Capturar los datos del lidar */
+void saveData(float angle, float distance){
+  Serial.print("dist: ");
+  Serial.print(distance);
+  Serial.print("   angle: ");
+  Serial.println(angle);
+}
+
+/* Leer el buffer del serial para capturar los angulos del posicionamiento manual o la rutina automatica*/
+void capturarAngulos(){
+  int separador;
+  String lectura_serial;
+  char array_serial[4], pan_array[2], tilt_array[2];
+  Serial.print("Aqui");
+  if(Serial.available() > 2){
+    lectura_serial = Serial.readString();
+    separador = lectura_serial.indexOf(',');
+    lectura_serial.substring(0,separador).toCharArray(array_serial,4);
+    if(array_serial[0] == '-'){
+      pan_array[0] = array_serial[1];
+      pan_array[1] = array_serial[2];
+      pan = atoi(pan_array);
+      pan = pan * -1;    
+    }
+    else{
+      pan = atoi(array_serial);
+    }
+    lectura_serial.substring(separador+1,lectura_serial.length()).toCharArray(array_serial,4);
+    if(array_serial[0] == '-'){
+      tilt_array[0] = array_serial[1];
+      tilt_array[1] = array_serial[2];
+      tilt = atoi(tilt_array);
+      tilt = tilt * -1;    
+    }
+    else{
+      tilt = atoi(array_serial);
+    }
+    pan += 95;
+    tilt += 95;
+    Serial.print(pan);
+    Serial.print(",");
+    Serial.println(tilt);
+  }
+}
+
+/*Mover un servo a un angulo especifico*/
 void moverServo(uint8_t servo, uint8_t angulo) {
 
   int ancho_pulso_crudo, ancho_pulso_convertido;
@@ -31,36 +82,38 @@ void moverServo(uint8_t servo, uint8_t angulo) {
   pwmServos.setPWM(servo, 0, ancho_pulso_convertido);
 }
 
+/*Ejecutar el movimiento de los servos a una posicion especifica (Modo manual)*/
 void posicionManual(uint8_t angulo_pan, uint8_t angulo_tilt){
   moverServo(servo_pan,angulo_pan);
   moverServo(servo_tilt,angulo_tilt);
 }
 
+/*Ejecutar el movimiento de los servos a una rutina especificada (Modo automatico)*/
+void iniciarRutina(){ // Falta añadir codigo
+
+}
+
+/*Ejecutar el movimiento de los servos a la posicion home (Tilt - 95°, Pan - 90°)*/
 void homeTilt(){
-  moverServo(servo_pan,0);
-  moverServo(servo_tilt,0);
+  moverServo(servo_pan,90);
+  moverServo(servo_tilt,95);
 }
 
-void ascensoMastil(){
- // Aqui debe ir el codigo para el ascenso del mastil
+/*Control PID para el ascenso y descenso del mastil (POR IMPLEMENTAR)*/
+void baseMastil(int task){
+  if(task == 'a'){
+    // Ascenso
+  }
+  else if(task == 'd'){
+    // Descenso
+  }
+  else{
+    // Detener
+  }
 }
 
-void descensoMastil(){
-  // Aqui debe ir el codigo para el descenso del mastil
-}
-
-void iniciarRutina(){
-
-}
-
-void saveData(float angle, float distance){
-  Serial.print("dist: ");
-  Serial.print(distance);
-  Serial.print("   angle: ");
-  Serial.println(angle);
-}
-
-void lidarScan(){
+/*Escaneo de una muestra del sensor Lidar*/
+void lidarScan(){// Modificar inicio Motor
     if (IS_OK(lidar.waitPoint())) {
       float distance = lidar.getCurrentPoint().distance;
       float angle = lidar.getCurrentPoint().angle;  // 0-360 deg
@@ -83,57 +136,60 @@ void lidarScan(){
       if (IS_OK(lidar.getDeviceInfo(info, 500))) {
         // Detected
         lidar.startScan();
-        pwmLidar.setPWM(RPLIDAR_MOTOR,2048,0);
-        delay(500);
+        digitalWrite(RPLIDAR_MOTOR_IN1, HIGH);
+        analogWrite(RPLIDAR_MOTOR_EN,128);
+        // pwmServos.setPWM(RPLIDAR_MOTOR,2048,0);
+        // delay(500);
         Serial.print("READY");
       }
     }
 }
 
-void lidarStop(){
-  pwmLidar.setPWM(RPLIDAR_MOTOR,0,4096);
+/*Detener el escaneo del sensor Lidar*/
+void lidarStop(){ // Modificar inicio Motor
+  // pwmServos.setPWM(RPLIDAR_MOTOR,2048,0);
   lidar.stop();
   Serial.print("STOPPED");
 }
 
-void lidarSave(){
+/*Envio de los datos escaneados por el Lidar al Serial*/
+void lidarSave(){ // Modificar guardado de datos
   // Codigo para enviar los datos del lidar
 }
 
+/*Interpretador de comandos enviados por la HMI*/
 void interpretador(byte comando){
   switch(comando){
-    case 0x01:
+    case 0x01: // Iniciar Escaneo
       lidarScan();
       break;
-    case 0x02:
+    case 0x02: // Detener Escaneo
       lidarStop();
       break;
-    case 0x03:
+    case 0x03: // Guardar Datos
       Serial.print(0x03);
       //lidarSave();
       break;
-    case 0x04:
+    case 0x04: // Modo Automatico: Iniciar Rutina
       Serial.print(0x04);
       //iniciarRutina();
       break;
-    case 0x05:
-      Serial.print(0x05);
-      //posicionManual();
+    case 0x05: // Modo Manual: Enviar
+      capturarAngulos();
+      posicionManual(pan,tilt);
       break;
-    case 0x06:
-      Serial.print(0x06);
-      //homeTilt();
+    case 0x06: // Modo Manual: Home
+      homeTilt();
       break;
-    case 0x07:
+    case 0x07: // Modo Manual: Ascenso
       Serial.print(0x07);
-      //ascensoMastil();
+      //baseMastil('a');
       break;
-    case 0x08:
+    case 0x08: // Modo Manual: Descenso
       Serial.print(0x08);
-      //descensoMastil();
+      //baseMastil('d');
       break;
     default:
-      Serial.println("0x00");
       break;
   }
 }
@@ -141,16 +197,16 @@ void interpretador(byte comando){
 void setup(){
   pwmServos.begin(); // Iniciar el driver PCA9685
   pwmServos.setPWMFreq(FREQUENCY); // Establecer la frecuencia del servo
-  pwmServos.setOscillatorFrequency(27000000); // Establecer la frecuencia del oscilador del driver PCA9685
-  
-  pwmLidar.begin(); // Iniciar el driver PCA9685
-  pwmLidar.setPWMFreq(1600); // Establecer la frecuencia del servo
-  pwmLidar.setOscillatorFrequency(27000000); // Establecer la frecuencia del oscilador del driver PCA9685
-  
+  pwmServos.setOscillatorFrequency(27000000); // Establecer la frecuencia del oscilador del driver PCA9685 para los Servos
+  homeTilt();
+  //pwmLidar.begin(); // Iniciar el driver PCA9685
+  //pwmLidar.setPWMFreq(1600); // Establecer la frecuencia del servo
+  //pwmLidar.setOscillatorFrequency(27000000); // Establecer la frecuencia del oscilador del driver PCA9685 para el Lidar
   Serial.begin(460800);
   Serial2.begin(115200);
   lidar.begin(Serial2);
-  
+  pinMode(RPLIDAR_MOTOR_EN, OUTPUT);
+  pinMode(RPLIDAR_MOTOR_IN1, OUTPUT);
 }
 
 void loop(){
